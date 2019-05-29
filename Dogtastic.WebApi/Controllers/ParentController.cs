5ead28d5102aeb2a58ce994ac1c7778d20873bf4
DeeNicely <dbirky6520@gmail.com>
2019-05-29 08:58:49 -0400

    Add API controller

---
+++ Dogtastic.WebApi/Controllers/ParentController.cs
@@ -0,0 +1,28 @@
+﻿using Dogtastic.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Dogtastic.WebApi.Controllers
+{
+    [Authorize]
+    public class ParentController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            ParentService parentService = CreateParentService();
+            var notes = parentService.GetParents();
+            return Ok(notes);
+        }
+        private ParentService CreateParentService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var parentService = new ParentService(userId);
+            return parentService;
+        }
+    }
+}
